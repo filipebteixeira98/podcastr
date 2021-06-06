b2be71e91a6419583984426c3885cf6a5e592040
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-06-06 15:00:34 -0300

    feat: format retrieved data from fake API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import { GetStaticProps } from 'next'
+import { format, parseISO } from 'date-fns'
+import enUS from 'date-fns/locale/en-US'
 
 import { api } from '../services/api'
+
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
 
 type Episode = {
   id: string
   title: string
+  thumbnail: string
+  description: string
+  duration: number
+  durationAsString: string
   members: string
+  publishedAt: string
+  url: string
 }
 
 type HomeProps = {
@@ -29,10 +39,24 @@ export const getStaticProps: GetStaticProps = async () => {
       _order: 'desc',
     },
   })
+  
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'MMM d yy', { locale: enUS }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   }

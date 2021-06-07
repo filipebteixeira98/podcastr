07bbc794c77e182274d99c8ce725af82e83db99a
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-06-07 12:44:14 -0300

    feat: add prefetch & ISR through getStaticPaths

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -29,7 +29,11 @@ type EpisodeProps = {
 
 export default function Episode({ episode }: EpisodeProps) {
   // const router = useRouter()
+  // if (router.isFallback) {
+  //   return <p>Loading...</p>
+  // }
   // return <h1>{router.query.slug}</h1>
+  
   return (
     <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
@@ -60,8 +64,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('/episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  })
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id,
+      },
+    }
+  })
+  
   return {
-    paths: [],
+    paths,
     fallback: 'blocking',
   }
 }
